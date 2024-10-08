0ebfde0798f17c1d4bb9bf53c0a52166f9353329
Hayden Frentzel <htfrentzel@gmail.com>
2024-10-08 18:26:13 -0500

    Add delta, fd, and zoxide

--- scripts/update_hole.py
+++ scripts/update_hole.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python
 import json
+from urllib.parse import quote
 
 with open("hole.json") as f:
     hole = json.loads(f.read())
 
 with open("manifest.txt") as f:
-    manifest = f.readlines()
+    manifest = [r.strip() for r in f.readlines()]
 
 release, *files = manifest
 for file in files:
@@ -17,7 +18,7 @@ for file in files:
 
     env = env.split(".tar.gz")[0]
     hole[app][version][env] = (
-        f"https://github.com/hfrentzel/build-hole/releases/download/{release.strip()}/{file.strip()}"
+        f"https://github.com/hfrentzel/build-hole/releases/download/{quote(release)}/{file}"
     )
 
 with open("hole.json", "w") as f:
